7b02d398a26fe0ecb3de2edab21e0b5d7e59bfe5
Hanjt <18251979626@163.com>
2020-05-09 10:08:24 +0800

    solve Binary Tree Level Order Traversal

--- src/main/scala/com/test/leetcode/TreeSolutions.scala
+++ src/main/scala/com/test/leetcode/TreeSolutions.scala
@@ -1,5 +1,7 @@
 package com.test.leetcode
 
+import scala.collection.mutable.ListBuffer
+
 class TreeSolutions {
 
   //144.二叉树的前序遍历
@@ -10,12 +12,12 @@ class TreeSolutions {
     var right: TreeNode = null
   }
 
-    //Recursive solution
-    def preorderTraversalRecursive(root: TreeNode): List[Int] = {
-      var result = scala.collection.mutable.ListBuffer[Int]()
-      helper(root, result)
-      result.toList
-    }
+  //Recursive solution
+  def preorderTraversalRecursive(root: TreeNode): List[Int] = {
+    var result = scala.collection.mutable.ListBuffer[Int]()
+    helper(root, result)
+    result.toList
+  }
 
   def helper(root: TreeNode, result: scala.collection.mutable.ListBuffer[Int]): scala.collection.mutable.ListBuffer[Int] = {
     if (root != null) {
@@ -41,4 +43,26 @@ class TreeSolutions {
     }
     result.toList
   }
+
+  //102. 二叉树的层序遍历
+  def levelOrder(root: TreeNode): List[List[Int]] = {
+    var result: scala.collection.mutable.ListBuffer[List[Int]] = scala.collection.mutable.ListBuffer()
+    if (root == null) return result.toList
+    val queue = scala.collection.mutable.Queue.empty[TreeNode]
+    var level = 0
+    queue.enqueue(root)
+    while (queue.nonEmpty) {
+      result.addOne(List[Int]())
+      for (i <- queue.indices) {
+        val node = queue.dequeue()
+        result(level) = result(level).appended(node.value)
+        if (node.left != null) queue.addOne(node.left)
+        if (node.right != null) queue.addOne(node.right)
+      }
+      level += 1
+    }
+    result.toList
+  }
+
+
 }
