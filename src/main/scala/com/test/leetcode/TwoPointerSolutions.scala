cc955c54e74b57b12ce3149963c7c8c3c6481f9d
han <18251979626@163.com>
2020-06-17 22:32:44 +0800

    add three sum

--- src/main/scala/com/test/leetcode/TwoPointerSolutions.scala
+++ src/main/scala/com/test/leetcode/TwoPointerSolutions.scala
@@ -1,5 +1,6 @@
 package com.test.leetcode
 
+
 class TwoPointerSolutions {
   //26.删除排序数组中的重复项
   def removeDuplicates(nums: Array[Int]): Int = {
@@ -35,12 +36,33 @@ class TwoPointerSolutions {
     val len = s.length
     var i = 0
     var j = len - 1
-    while (i < j){
+    while (i < j) {
       var tmp = s(i)
       s(i) = s(j)
       s(j) = tmp
       i += 1
       j -= 1
     }
+  }
+
+  //15. 三数之和
+  def threeSum(nums: Array[Int]): List[List[Int]] = {
+    import scala.collection.mutable.ListBuffer
+    var result: ListBuffer[List[Int]] = ListBuffer(List())
+    val sortedNums = nums.sorted
+    for (first <- sortedNums.indices if first <= 0 || sortedNums(first) != sortedNums(first - 1)) {
+      val target = -sortedNums(first)
+      var third = sortedNums.length - 1
+      var flag = true
+      for (second <- (first + 1) until sortedNums.length if second <= first + 1 || sortedNums(second) != sortedNums(second - 1) && flag) {
+        while (second < third && sortedNums(second) + sortedNums(third) > target) {
+          third -= 1
+        }
+        if (second == third) flag = false
+        else if (sortedNums(second) + sortedNums(third) == target) result.addOne(List(sortedNums(first), sortedNums(second), sortedNums(third)))
+      }
+
+    }
+    result.toList.filter(_!=List())
   }
 }
